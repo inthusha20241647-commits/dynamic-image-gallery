1999daa661d5e2f0a954a4b57ca05d045795b696
inthusha20241647-commits <inthusha.20241647@iit.ac.lk>
2026-09-25 15:16:48 +0530

    create dynamic image gallery with react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+import morning from "./images/morning.jpg";
+import mountain from "./images/mountain.jpg";
+import waterfall from "./images/waterfall.jpg";
+import landscape from "./images/landscape.jpg";
+import spring from "./images/spring.jpg";
+import evening from "./images/evening.jpg";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Display(props) {
+  return (
+    
+      <div className="ImageCard">
+        <img src={props.image} alt={props.title}></img>
+        <h2>{props.title}</h2>
+        <p>{props.desc}</p>
+      </div>
+    
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const gallery_profile = [
+  {
+    image: morning,
+    title: "morning",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+  },
+  {
+    image: mountain,
+    title: "mountain",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+  },
+  {
+    image: waterfall,
+    title: "waterfall",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+  },
+  {
+    image: landscape,
+    title: "landscape",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+  },
+  {
+    image: evening,
+    title: "evening",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+  },
+  {
+    image: spring,
+    title: "Spring",
+    desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit voluptate velit esse cillum dolore eu fugiat nulla pariatur.`,
+  },
+];
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <div className="container">
+    {gallery_profile.map(function (item) {
+      return <Display image={item.image} title={item.title} desc={item.desc}></Display>;
+    })}
+    </div>
+  </div>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
